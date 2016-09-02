c699bca6e110b977f5a55b82b29b65ece5ecc233
hsuj6917 <john.hsu.penguin@gmail.com>
2016-09-02 11:57:25 -0700

    ASCIIart2

---
+++ APLesson_01/ASCIIart2.java
@@ -0,0 +1,25 @@
+public class ASCIIart2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("             <3                 ");
+		System.out.println("             <3                 ");
+		System.out.println("         <3<3<3<3<3             ");
+		System.out.println("       <3    <3    <3           ");
+		System.out.println("     <3      <3      <3         ");
+		System.out.println("     <3      <3                 ");
+		System.out.println("     <3      <3                 ");
+		System.out.println("      <3     <3                 ");
+		System.out.println("       <3    <3                 ");
+		System.out.println("         <3<3<3<3<3             ");
+		System.out.println("             <3    <3           ");
+		System.out.println("             <3      <3         ");
+		System.out.println("             <3      <3         ");
+		System.out.println("             <3      <3         ");
+		System.out.println("     <3      <3     <3          ");
+		System.out.println("       <3    <3    <3           ");
+		System.out.println("         <3<3<3<3<3             ");
+		System.out.println("             <3                 ");
+		System.out.println("             <3                 ");
+	}  
+}
