a24f918498b1ca9da43125ad2a547b1e622cbeea
hsuj6917 <john.hsu.penguin@gmail.com>
2016-09-02 11:40:00 -0700

    ASCIIart1

--- APLesson_01/ASCIIart1.java
+++ APLesson_01/ASCIIart1.java
@@ -2,6 +2,24 @@ public class ASCIIart1
 {
 	public static void main(String[]args)
 	{
-		System.out.println("                              $");
+		System.out.println("                                ");
+		System.out.println("                                   ");
+		System.out.println("                                ");
+		System.out.println("          $$$$         $$$$");
+		System.out.println("        $$$$$$$       $$$$$$$");
+		System.out.println("       $$$$$$$$$     $$$$$$$$$");
+		System.out.println("      $$$$$$$$$$$   $$$$$$$$$$$");
+		System.out.println("      $$$$$$$$$$$$ $$$$$$$$$$$$");
+		System.out.println("      $$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("       $$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("        $$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("         $$$$$$$$$$$$$$$$$$$");
+		System.out.println("          $$$$$$$$$$$$$$$$$");
+		System.out.println("           $$$$$$$$$$$$$$$");
+		System.out.println("             $$$$$$$$$$$");
+		System.out.println("               $$$$$$$");
+		System.out.println("                 $$$");
+		System.out.println("                              ");
+
 	}
 }
