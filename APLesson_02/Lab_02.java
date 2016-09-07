6e51dfc0ee08064800fc1a4a7ab9cccf996da95a
hsuj6917 <john.hsu.penguin@gmail.com>
2016-09-07 12:11:29 -0700

    9/7/2016

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,20 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 7;
+		int b = 7;
+		int product = a * b;
+		String fname = "Barack";
+		String lname = "Obama";
+		int address0 = 1600;
+		String address1 = "Pennsylvania";
+		String address2 = "Avenue";
+		String city1 = "Washington";
+		String city2 = "DC";
+		int ZIP = 20500;
+		
+		System.out.println(a + " multiplied by " + b + " is" +" "+ product);
+		System.out.println(fname + " " + lname + "\n" + address0 + " " + address1 + " " + address2 + "\n" + city1 + " " + city2 + " " + ZIP);
+	}
+}
---
+++ APLesson_02/Lesson_02reference.java
@@ -0,0 +1,8 @@
+public class Lesson_02reference
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+	}
+}
