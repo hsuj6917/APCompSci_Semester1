7ef61e84a65473b8aaa4ff9966841a2c34dd9dda
hsuj6917 <john.hsu.penguin@gmail.com>
2016-09-13 11:03:56 -0700

    9/13

--- APLesson_03/BMI.java
+++ APLesson_03/BMI.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class BMI
 {
 	public static void main(String[]args)
 	{
+		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Welcome to the BMI calculator! ");
 		System.out.println("How much do you weigh in lbs? ");
 		int weight = keyboard.nextInt();
@@ -12,6 +15,6 @@ public class BMI
 		int d = height * height;
 		int bmi = n / d;
 		
-		System.out.println("Your BMI is " + bmi + ". Let me connect you with a personal trainer. ");
+		System.out.println("Congratulations! Your BMI is " + bmi + ".");
 	}
 }
