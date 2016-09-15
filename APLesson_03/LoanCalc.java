a68a334e258cb265c25484cc94ef86542b0f4411
hsuj6917 <john.hsu.penguin@gmail.com>
2016-09-15 11:51:02 -0700

    9/15

--- APLesson_03/LoanCalc.java
+++ APLesson_03/LoanCalc.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class MortgageCalc
+public class LoanCalc
 {
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Welcome to the Mortgage Payment Calculator. Please enter your loan amount. ");
+		System.out.println("Welcome to the Mortgage Payment Calculator.\nThis might be the wrong formula but it works.\n Please enter your loan amount. ");
 		int P = kb.nextInt();
 		System.out.println("Enter the monthly interest rate, in decimal form. ");
 		double i = kb.nextDouble();
 		System.out.println("Enter the length of the loan in months. ");
 		int L = kb.nextInt();
 		double para = 1 + i;
-		double exp = L * -1;
-		double pow = Math.pow(para,exp);
-		double denom = 1 - pow;
-		double M = (i / denom) * 100;
-		System.out.printf("Your monthly payment is $%.2f.", M );
+		double pow = Math.pow(para,L);
+		double num = i * pow;
+		double den = pow - 1;
+		double M = (num / den) * P;
+		System.out.printf("Your monthly payment is $%.2f", M );
 		
 	}
 }
