09ffe7b5309057e94880785357bf12e89e05587d
hsuj6917 <john.hsu.penguin@gmail.com>
2017-01-12 11:44:13 -0800

    1/12

--- APLesson_10/KeepComposites.java
+++ APLesson_10/KeepComposites.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -6,9 +5,33 @@ public class KeepComposites
 {
 	public static void main(String[]args)
 	{
-		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
-		ArrayList<Integer> nums = new ArrayList<>(Array.asList(numbers));
-		
+		Integer[] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
+		ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(numbers));
 		System.out.println(nums);
+		System.out.println("W/out primes:: " + removePrimes(nums));
+	}
+	
+	public static int gFactor(int n)
+	{
+		for(int i=2; i<n; i++)
+		{
+			if(n % i == 0)
+				return 1;
+			return 0;
+		}
+		return 0;
+	}
+	
+	public static ArrayList<Integer> removePrimes(ArrayList<Integer> nums)
+	{
+		for(int i=0; i < nums.size(); i++)
+		{
+			if(gFactor(nums.get(i)) == 0)
+			{
+				nums.remove(i);
+				i--;
+			}
+		}
+		return nums;
 	}
 }
