9dbdcabc00237b5cc37e647db576e89e83e09546
hsuj6917 <john.hsu.penguin@gmail.com>
2017-01-19 11:54:23 -0800

    1/19

--- APLesson_10/ExpressionSolver.java
+++ APLesson_10/ExpressionSolver.java
@@ -9,22 +9,50 @@ public class ExpressionSolver
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Enter an equation: ");
 		String expression = kb.nextLine();
-		
 		ArrayList<String>equation = new ArrayList<String>(Arrays.asList(expression.split(" ")));
+		System.out.println(doEquation(equation));
+	}
 		
+	public static ArrayList<String> doEquation(ArrayList<String> equation)
+	{
 		int i = 0;
 		while(i<equation.size())
 		{
-			if(i<equation.size() && equation.get(i).equals("+"))
+			if(equation.get(i).equals("*") || equation.get(i).equals("/"))
 			{
-				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+				if (equation.get(i).equals("*")){
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * Integer.parseInt(equation.get(i+1))));			
+				}
+				else {
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / Integer.parseInt(equation.get(i+1))));
+	
+				}
 				equation.remove(i-1);
-				equation.remove(i);
+				equation.remove(i);	
 			}
-			else
-				i++;
+			
+			i++;
 		}
+		i = 0;
+		while(i<equation.size())
+		{
+			if(equation.get(i).equals("+") || equation.get(i).equals("-"))
+			{
+				if (equation.get(i).equals("+")){
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));			
+				}
+				else {
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - Integer.parseInt(equation.get(i+1))));
+	
+				}
+				equation.remove(i-1);
+				equation.remove(i);	
+			}
+			
+			i++;
+		}
+			
+		return equation;
+	}	
 		
-		System.out.println(equation);
-	}
 }
