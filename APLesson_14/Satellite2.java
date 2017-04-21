61ea5cd8c9e2c679be10cb988ea0e74a7de8195d
hsuj6917 <john.hsu.penguin@gmail.com>
2017-04-21 10:43:56 -0700

    last fix for 14

--- APLesson_14/Satellite2.java
+++ APLesson_14/Satellite2.java
@@ -4,16 +4,16 @@ public class Satellite2
    public static void main(String[]args)
    {
 	   
-       ArrayList<Location> locate = new ArrayList<>();
+       ArrayList<Location2> locate = new ArrayList<>();
        double[] honLoc = {5, 6};
-       locate.add(new Honda(honLoc));
-       locate.add(new Toyota("8, 9"));
-       locate.add(new GMC(3, 8));
+       locate.add(new Honda2(honLoc));
+       locate.add(new Toyota2("8, 9"));
+       locate.add(new GMC2(3, 8));
 
 
        double[] home = {0, 0};
    	 	 	
-     	for(Location l : locate)
+     	for(Location2 l : locate)
      	{
 			double x = (Math.round((Math.random()*100)*100.00))/100.00;
 			double y = (Math.round((Math.random()*100)*100.00))/100.00;
@@ -28,7 +28,7 @@ public class Satellite2
 		String printout = "\n\n" +
                "==========================" + "\nStarting locations...";
 			   
-			    for (Location l : locate)
+			    for (Location2 l : locate)
        {
            printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
        }
@@ -37,7 +37,7 @@ public class Satellite2
        printout += "\n\n" + "==========================" + "\nDistance from home...";
 
 
-       for (Location l : locate)
+       for (Location2 l : locate)
        {
 		   System.out.printf("\nDistance for " + l.getID() + ": %.2f", getDistance(l.getLoc(), home));
        }
